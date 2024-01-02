31829ecbb4e83194338e483acc07bc210559ff41
michel <michel-gouveia1997@hotmail.com>
2024-01-01 22:57:12 -0300

    first commit in a while

---
+++ js/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+
+navigator.geolocation.getCurrentPosition(function(positon){
+
+    const {latitude} = positon.coords
+    const {longitude} = positon.coords
+
+    const coords = [latitude,longitude]
+
+    const map = L.map('map').setView(coords, 16);
+
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+/*
+L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+    */
+    map.on('click',function(mapEvent){
+
+        const {lat,lng} = mapEvent.latlng;
+        
+        L.marker([lat,lng]).addTo(map)
+    .bindPopup('Workout')
+    .openPopup();
+    })
+
+},function(){
+    alert('hi')
+})
